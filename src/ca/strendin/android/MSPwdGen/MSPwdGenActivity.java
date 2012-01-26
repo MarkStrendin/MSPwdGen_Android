10f3ae9306e6ee9a9a2950abb92a7b75131b5bed
Mark Strendin <mark.strendin@gmail.com>
2012-01-25 22:09:18 -0600

    First commit, Started the project and created the basic layout.

---
+++ src/ca/strendin/android/MSPwdGen/MSPwdGenActivity.java
@@ -0,0 +1,14 @@
+package ca.strendin.android.MSPwdGen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MSPwdGenActivity extends Activity {
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
